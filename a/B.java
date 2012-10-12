977fd0699589e49a24dee325bd1b0e1b3a41178f
Kevin Downey <redchin@gmail.com>
2012-10-11 20:21:22 -0700

    library loading work arounds

--- a/B.java
+++ a/B.java
@@ -1,4 +1,11 @@
 package a;
 public class B {
     public native long mem(long offset, long size);
+    /* make sure the shared library is loaded by the same classloader as the class */
+    public static void loadLibrary(String l) {
+        System.loadLibrary(l);
+    }
+    public static void load(String l) {
+        System.load(l);
+    }
 }
