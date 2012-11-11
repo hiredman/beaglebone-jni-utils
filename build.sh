40a7af945d00a39810d7c9a03355e8cf6a479752
Kevin Downey <redchin@gmail.com>
2012-11-10 20:52:53 -0800

    more fiddling with the build script

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ JAR=$JAVA_HOME/bin/jar
 JAVAH=$JAVA_HOME/bin/javah
 INCLUDE=$JAVA_HOME/include
 ARCH=$(uname -s|tr A-Z a-z)
+LIBRARY_NAME="B"
+CLASSNAME="a.B"
 
 # clean up
 find $PWD -name \*.class -name \*.so -name \*.h -name \*.jar -name \*.xml -delete
@@ -14,11 +16,11 @@ find $PWD -name \*.class -name \*.so -name \*.h -name \*.jar -name \*.xml -delet
 # compile java files
 find $PWD -type f -name \*.java | xargs $JAVAC
 
-# generate jni headers
-$JAVAH -jni -classpath . -o B.h a.B
+# generate jni header
+$JAVAH -jni -classpath . -o ${LIBRARY_NAME}.h $CLASSNAME
 
 # compile jni code
-cc -shared -I$INCLUDE -I$INCLUDE/$ARCH/ B.c -o libB.so
+cc -shared -I$INCLUDE -I$INCLUDE/$ARCH/ ${LIBRARY_NAME}.c -o "lib"${LIBRARY_NAME}".so" 
 
 # generate pom
 cat > pom.xml << EOF
@@ -26,12 +28,12 @@ cat > pom.xml << EOF
 <project>
   <modelVersion>4.0.0</modelVersion>
   <groupId>$PROJECT_NAME</groupId>
-  <artifactId>$PROJECT_NAME</artifactId>
+   <artifactId>$PROJECT_NAME</artifactId>
   <version>$VERSION</version>
   <name>$PROJECT_NAME</name>
 </project>
 
 EOF
-
+ 
 # jar it all up
 $JAR -cf $PROJECT_NAME-$VERSION.jar *
